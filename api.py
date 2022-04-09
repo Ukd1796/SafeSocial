816170c6943c2ce3604a8a3242644ce5fa0e4e4c
Dhairya Ameria <71331819+dhairyaameria@users.noreply.github.com>
2022-04-09 22:17:04 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,27 +1,27 @@
 import re
 import string
 import nltk
-from text import Text
-from fastapi.responses import HTMLResponse
+import pickle
 nltk.download('stopwords')
 stemmer = nltk.SnowballStemmer("english")
 from nltk.corpus import stopwords
 stopword=set(stopwords.words('english'))
 import uvicorn
-from fastapi import FastAPI, HTTPException, Request, Form
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.templating import Jinja2Templates
-from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
 import keras
 from starlette.staticfiles import StaticFiles
-from starlette.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates/")
 app.mount("/static", StaticFiles(directory="./static"), name="static")
 
-load_model=keras.models.load_model("./hate&abusive_model.h5")
+load_model=keras.models.load_model("./Sentiment_Analysis.h5")
+with open('./Sentiment_Tokenizer.pickle', 'rb') as handle:
+   load_tokenizer1 = pickle.load(handle)
 
 def clean_text(text):
     print(text)
@@ -39,31 +39,35 @@ def clean_text(text):
     text=" ".join(text)
     return text
 
-@app.get('/')
-async def index(request: Request):
-    return templates.TemplateResponse("./index.html", {"request": request})
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post('/predict')
-async def predict(test: str = Form("Fuck you")): 
+async def predict(test: Request): 
+    print(test)
+    test = await test.json()
     if not test:
         raise HTTPException(status_code=404, detail="Sentence field is required")
     test=[clean_text(test)]
-    max_words = 50000
-    tokenizer = Tokenizer(num_words=max_words)
-    tokenizer.fit_on_texts(test)
-    seq = tokenizer.texts_to_sequences(test)
+    seq = load_tokenizer1.texts_to_sequences(test)
     padded = sequence.pad_sequences(seq, maxlen=300)
     pred = load_model.predict(padded)
     if pred<0.5:
         comment= "no hate"
     else:
         comment= "hate and abusive"
-    prediction_text='The comment is :{}'.format(comment)
-    return HTMLResponse(content=prediction_text, status_code=200)
-
+    return comment
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-
-#     # return FileResponse('./index.html',content=prediction_text)
-#  return templates.TemplateResponse("./index.html",content= {"request": request, "result":prediction_text})
