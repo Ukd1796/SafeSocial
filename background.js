26981ad8e0581c8202cc32f77640976ae8dd4235
Dhairya Ameria <71331819+dhairyaameria@users.noreply.github.com>
2022-04-09 23:31:32 +0530

    content and background updated.

--- background.js
+++ background.js
@@ -5,7 +5,7 @@ chrome.action.onClicked.addListener(buttonClicked)
 
 function buttonClicked(tab){
 
-    console.log(tab.url)
+    //console.log(tab.url)
 }
 
 var currentUrl,previousUrl;
@@ -16,24 +16,61 @@ setInterval(function(){
           currentUrl = tabs[0].url
           if(currentUrl!=previousUrl)
            {
-             console.log(response.msg);
+             //console.log(response.msg);
              previousUrl = currentUrl
           }
         });
       });
 },1000)
 
-
+var flag =0;
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-      console.log(request.textMsg)
-      sendResponse({farewell:"Recieved!!!"})
+   function(request, sender, sendResponse) {
+      // console.log(request.textMsg)
+   var res = sendResource('http://127.0.0.1:8000/predict',request.textMsg).then((data)=>{console.log(data)
+   return data 
+  })
+  console.log("res: ",res)
+      sendResponse({farewell:res})
   }
 );
-   
-
-
-
-
-
+var finalResult
+function sendResource(url,data) {
+  return fetch(url, {
+    method: 'POST',
+    body:JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+   return data; 
+  
+}
+)}
  
+
+
+
+
+// var flag =0;
+// chrome.runtime.onMessage.addListener(
+//    function(request, sender, sendResponse) {
+//       // console.log(request.textMsg)
+//    var res = sendResource('http://127.0.0.1:8000/predict',request.textMsg).then((data)=>{console.log(data)
+//    return data 
+//   })
+//   console.log("res: ",res)
+//       sendResponse({farewell:res})
+//   }
+// );
+// var finalResult
+// function sendResource(url,data) {
+//   return fetch(url, {
+//     method: 'POST',
+//     body:JSON.stringify(data)
+//   })
+//   .then(response => response.json())
+//   .then(data => {
+//    return data; 
+  
+// }
+// )}
